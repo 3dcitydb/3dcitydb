2734b71d6b0fd5a08e68499c0b39b515f6941e51
Felix Kunde <fkunde@virtualcitysystems.de>
2015-01-08 14:50:45 +0100

    [postgresql] minor fix in MIGRATE_DB_V2_V3.sql

--- PostgreSQL/SQLScripts/MIGRATION/MIGRATE_DB_V2_V3.sql
+++ PostgreSQL/SQLScripts/MIGRATION/MIGRATE_DB_V2_V3.sql
@@ -972,7 +972,8 @@ CREATE TABLE citydb.textureparam (
 WITH tex_image_entry AS (
   WITH tex_image_insert AS ( 
     SELECT sd_v2.tex_image_uri, sd_v2.tex_image, sd_v2.tex_mime_type
-      FROM public.surface_data sd_v2, (SELECT min(id) AS sample_id FROM public.surface_data GROUP BY tex_image_uri) sample
+      FROM public.surface_data sd_v2, 
+           (SELECT min(id) AS sample_id FROM public.surface_data WHERE tex_image_uri IS NOT NULL GROUP BY tex_image_uri) sample
       WHERE sd_v2.id = sample.sample_id
   )
   INSERT INTO citydb.tex_image (tex_image_uri, tex_image_data, tex_mime_type)
