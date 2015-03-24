89167f354653d21b13ebe23cbc96785acee186f3
Felix Kunde <fkunde@virtualcitysystems.de>
2015-03-24 10:19:51 +0100

    [oracle] updated header information

--- Oracle/SQLScripts/UTIL/CREATE_DB/HINT_ON_MISTYPED_DBVERSION.sql
+++ Oracle/SQLScripts/UTIL/CREATE_DB/HINT_ON_MISTYPED_DBVERSION.sql
@@ -1,26 +1,10 @@
 -- HINT_ON_MISTYPED_DBVERSION.sql
 --
--- Authors:     Prof. Dr. Lutz Pluemer <pluemer@ikg.uni-bonn.de>
---              Prof. Dr. Thomas H. Kolbe <thomas.kolbe@tum.de>
---              Dr. Gerhard Groeger <groeger@ikg.uni-bonn.de>
---              Joerg Schmittwilken <schmittwilken@ikg.uni-bonn.de>
---              Viktor Stroh <stroh@ikg.uni-bonn.de>
---              Dr. Andreas Poth <poth@lat-lon.de>
-
--- Copyright:   (c) 2012-2014  Chair of Geoinformatics,
+-- Authors:     Zhihang Yao <zhihang.yao@tum.de>
+--
+-- Copyright:   (c) 2012-2015  Chair of Geoinformatics,
 --                             Technische Universität München, Germany
 --                             http://www.gis.bv.tum.de
---
---              (c) 2007-2012  Institute for Geodesy and Geoinformation Science,
---                             Technische Universität Berlin, Germany
---                             http://www.igg.tu-berlin.de
-
---              (c) 2004-2007  Institute for Cartography and Geoinformation,
---                             Universität Bonn, Germany
---                             http://www.ikg.uni-bonn.de
-
---              (c) 2005-2007  lat/lon GmbH, Germany
---                             http://www.lat-lon.de
 --
 --              This skript is free software under the LGPL Version 2.1.
 --              See the GNU Lesser General Public License at
@@ -33,7 +17,7 @@
 -- ChangeLog:
 --
 -- Version | Date       | Description                               | Author
--- 3.0.0     2015-03-05   added support for Oracle Locator            ZYao
+-- 1.0.0     2015-03-05   added support for Oracle Locator            ZYao
 --
 
 SET FEEDBACK OFF
