6b0c6a07fc3a1c086ba55d83e1b217c9ee7a93d6
Bruno Willenborg <b.willenborg@tum.de>
2018-02-27 21:15:24 +0100

    using parameters for srid, gmlsrsname in CREATE_DB.sql
    - moved prompt for srid, gmlsrsname to CREATE_DB.sh/.bat

--- PostgreSQL/SQLScripts/CREATE_DB.bat
+++ PostgreSQL/SQLScripts/CREATE_DB.bat
@@ -1,17 +1,50 @@
-REM Shell script to create an instance of the 3D City Database
-REM on PostgreSQL/PostGIS
+@echo off
+:: Shell script to create an instance of the 3D City Database
+:: on PostgreSQL/PostGIS
 
-REM Provide your database details here
+:: Provide your database details here
 set PGPORT=5432
 set PGHOST=your_host_address
 set PGUSER=your_username
 set CITYDB=your_database
 set PGBIN=path_to_psql.exe
 
-REM cd to path of the shell script
+:: cd to path of the shell script
 cd /d %~dp0
 
-REM Run CREATE_DB.sql to create the 3D City Database instance
-"%PGBIN%\psql" -d "%CITYDB%" -f "CREATE_DB.sql"
+:: Prompt for srsno
+:srid_retry
+set var=
+echo.
+echo Please enter a valid SRID (e.g., 3068 for DHDN/Soldner Berlin).
+echo Press ENTER to use default.
+set /p var="(default=3068): "
 
-pause
+IF /i NOT "%var%"=="" (
+  set srsno=%var%
+) else (
+  set srsno=3068
+)
+
+:: SRID is numeric?
+SET "num="&for /f "delims=0123456789" %%i in ("%var%") do set num=%%i
+IF defined num (
+  echo.
+  echo SRID must be numeric. Please retry.
+  goto:srid_retry
+)
+
+:: Prompt for gmlsrsname
+echo.
+echo Please enter the corresponding SRSName to be used in GML exports (e.g., urn:ogc:def:crs,crs:EPSG::3068,crs:EPSG::5783).
+echo Press ENTER to use default.
+set /p var="(default=urn:ogc:def:crs,crs:EPSG::3068,crs:EPSG::5783): "
+
+IF /i NOT "%var%"=="" (
+  set gmlsrsname=%var%
+) else (
+  set gmlsrsname=urn:ogc:def:crs,crs:EPSG::3068,crs:EPSG::5783
+)
+
+:: Run CREATE_DB.sql to create the 3D City Database instance
+"%PGBIN%\psql" -d "%CITYDB%" -f "CREATE_DB.sql" -v srsno="%srsno%" -v gmlsrsname="%gmlsrsname%"
--- PostgreSQL/SQLScripts/CREATE_DB.sh
+++ PostgreSQL/SQLScripts/CREATE_DB.sh
@@ -9,8 +9,31 @@ export PGUSER=your_username
 export CITYDB=your_database
 export PGBIN=path_to_psql
 
+# Prompt for SRSNO
+regex='^[0-9]+$'
+while [ 1 ]; do
+  echo
+  echo 'Please enter a valid SRID (e.g., 3068 for DHDN/Soldner Berlin).'
+  echo 'Press ENTER to use default.'
+  read -p "(default=3068): " SRSNO
+  SRSNO=${SRSNO:-3068}
+   
+  if [[ ! $SRSNO =~ $regex ]]; then
+    echo 'SRSNO must be numeric. Please retry.'
+  else 
+    break;
+  fi
+done
+
+# Prompt for GMLSRSNAME
+echo
+echo 'Please enter the corresponding SRSName to be used in GML exports (e.g., urn:ogc:def:crs,crs:EPSG::3068,crs:EPSG::5783).'
+echo 'Press ENTER to use default.'
+read -p '(default=urn:ogc:def:crs,crs:EPSG::3068,crs:EPSG::5783): ' GMLSRSNAME
+GMLSRSNAME=${GMLSRSNAME:-urn:ogc:def:crs,crs:EPSG::3068,crs:EPSG::5783}
+
 # cd to path of the shell script
 cd "$( cd "$( dirname "$0" )" && pwd )" > /dev/null
 
 # Run CREATE_DB.sql to create the 3D City Database instance
-"$PGBIN/psql" -d "$CITYDB" -f "CREATE_DB.sql"
+"$PGBIN/psql" -d "$CITYDB" -f "CREATE_DB.sql" -v srsno="$SRSNO" -v gmlsrsname="$GMLSRSNAME"
