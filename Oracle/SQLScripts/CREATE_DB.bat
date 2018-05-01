dd02c99c11a9c2ac3bc786cdfdf4d2dcc7a6e633
Bruno Willenborg <b.willenborg@tum.de>
2018-05-01 13:56:45 +0200

    fixed oracle CREATE_DB, DROP_DB scripts

--- Oracle/SQLScripts/CREATE_DB.bat
+++ Oracle/SQLScripts/CREATE_DB.bat
@@ -2,12 +2,10 @@
 :: Shell script to create an instance of the 3D City Database
 :: on Oracle Spatial/Locator
 
-:: Provide your database details here
+:: Provide your database details here -----------------------------------------
 set PORT=1521
 set HOST=your_host_address
 set USERNAME=your_username
-:: for testing only, to be removed!
-set PASSWORD=your_password
 set SID=your_SID_or_database_name
 set SQLPLUSBIN=path_to_sqlplus
 set SRSNO=
@@ -16,7 +14,7 @@ set GMLSRSNAME=
 set VERSIONING=no 
 :: DB Version (S/L) for Oracle Spatial or Oracle Locator
 set DBVERSION=S
-
+::-----------------------------------------------------------------------------
 
 :: add sqlplus to PATH
 set PATH=%PATH%;%SQLPLUSBIN%
@@ -24,11 +22,4 @@ set PATH=%PATH%;%SQLPLUSBIN%
 cd /d %~dp0
 
 :: Run CREATE_DB.sql to create the 3D City Database instance
-
-:: Opt1: This works for me
 sqlplus "%USERNAME%@\"%HOST%:%PORT%/%SID%\"" @CREATE_DB.sql "%SRSNO%" "%GMLSRSNAME%" "%VERSIONING%" "%DBVERSION%"
-
-:: Opt2: -Preferable, as password and username are prompted by sqlplus, not working for me
-::sqlplus "%HOST%:%PORT%/%SID%" @CREATE_DB.sql "%SRSNO%" "%GMLSRSNAME%" "%VERSIONING%" "%DBVERSION%"
-
-pause
--- Oracle/SQLScripts/CREATE_DB.sh
+++ Oracle/SQLScripts/CREATE_DB.sh
@@ -6,8 +6,6 @@
 export PORT=1521
 export HOST=your_host_address
 export USERNAME=your_username
-# for testing only, to be removed!
-export PASSWORD=your_password
 export SID=your_SID_or_database_name
 export SQLPLUSBIN=path_to_sqlplus
 export SRSNO=
@@ -16,7 +14,7 @@ export GMLSRSNAME=
 export VERSIONING=no 
 # DB Version (S/L) for Oracle Spatial or Oracle Locator
 export DBVERSION=S
-# -----------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 
 # add sqlplus to PATH
 export PATH=$PATH:$SQLPLUSBIN
@@ -25,12 +23,4 @@ export PATH=$PATH:$SQLPLUSBIN
 cd "$( cd "$( dirname "$0" )" && pwd )" > /dev/null
 
 # Run CREATE_DB.sql to create the 3D City Database instance
-
-# Opt1: This works for me
-sqlplus -S "${USERNAME}/${PASSWORD}@${HOST}:${PORT}/${SID}" @CREATE_DB.sql "${SRSNO}" "${GMLSRSNAME}" "${VERSIONING}" "${DBVERSION}"
-
-# Opt2: -Preferable, as password and username are prompted by sqlplus, not working for me
-#sqlplus "${HOST}:${PORT}/${SID}" @CREATE_DB.sql "${SRSNO}" "${GMLSRSNAME}" "${VERSIONING}" "${DBVERSION}"
-
-echo 'Press ENTER to quit!'
-read
+sqlplus "${USERNAME}@\"${HOST}:${PORT}/${SID}\"" @CREATE_DB.sql "${SRSNO}" "${GMLSRSNAME}" "${VERSIONING}" "${DBVERSION}"
