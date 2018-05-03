902b597f71f87209e5bb8724c65797568e444bd3
Bruno Willenborg <b.willenborg@tum.de>
2018-05-03 19:33:19 +0200

    finalized PostgreSQL/PostGIS CREATE_DB.sh
    - psql defined in script preferred to psql on PATH
    - some minor changes

--- PostgreSQL/SQLScripts/CREATE_DB.sh
+++ PostgreSQL/SQLScripts/CREATE_DB.sh
@@ -2,38 +2,44 @@
 # Shell script to create an instance of the 3D City Database
 # on PostgreSQL/PostGIS
 
-# Provide your database details here
-export PGPORT=5432
-export PGHOST=your_host_address
-export PGUSER=your_username
-export CITYDB=your_database
+# Provide your database details here ------------------------------------------
 export PGBIN=path_to_psql
+export PGHOST=your_host_address
+export PGPORT=5432
+export CITYDB=your_database
+export PGUSER=your_username
+#------------------------------------------------------------------------------
 
-# Prompt for SRSNO
-regex='^[0-9]+$'
+# add psql to PATH
+PATH=$PGBIN:$PATH
+
+# cd to path of the shell script
+cd "$( cd "$( dirname "$0" )" && pwd )" > /dev/null
+
+# Prompt for SRSNO ------------------------------------------------------------
+re='^[0-9]+$'
 while [ 1 ]; do
   echo
-  echo 'Please enter a valid SRID (e.g., 25832 for ETRS 89 / UTM zone 32N).'
-  echo 'Press ENTER to use default.'
-  read -p "(default=0): " SRSNO
-  SRSNO=${SRSNO:-0}
+  echo 'Please enter a valid SRID (e.g., 3068 for DHDN/Soldner Berlin): Press ENTER to use default.'
+  read -p "(default SRID=3068): " SRSNO
+  SRSNO=${SRSNO:-3068}
    
-  if [[ ! $SRSNO =~ $regex ]]; then
-    echo 'SRSNO must be numeric. Please retry.'
+  if [[ ! $SRSNO =~ $re ]]; then
+    echo 'SRID must be numeric. Please retry.'
   else 
     break;
   fi
 done
 
-# Prompt for GMLSRSNAME
+# Prompt for GMLSRSNAME -------------------------------------------------------
 echo
-echo 'Please enter the corresponding SRSName to be used in GML exports (e.g., urn:ogc:def:crs,crs:EPSG::25832,crs:EPSG::7837).'
-echo 'Press ENTER to use default.'
-read -p '(default=urn:ogc:def:crs,crs:EPSG::25832,crs:EPSG::7837): ' GMLSRSNAME
-GMLSRSNAME=${GMLSRSNAME:-urn:ogc:def:crs,crs:EPSG::25832,crs:EPSG::7837}
+echo 'Please enter the corresponding SRSName to be used in GML exports. Press ENTER to use default.'
+read -p '(default GMLSRSNAME=urn:ogc:def:crs,crs:EPSG:6.12:3068,crs:EPSG:6.12:5783): ' GMLSRSNAME
+GMLSRSNAME=${GMLSRSNAME:-urn:ogc:def:crs,crs:EPSG:6.12:3068,crs:EPSG:6.12:5783}
 
-# cd to path of the shell script
-cd "$( cd "$( dirname "$0" )" && pwd )" > /dev/null
+# Run CREATE_DB.sql to create the 3D City Database instance -------------------
+psql -d "$CITYDB" -f "CREATE_DB.sql" -v srsno="$SRSNO" -v gmlsrsname="$GMLSRSNAME"
 
-# Run CREATE_DB.sql to create the 3D City Database instance
-"$PGBIN/psql" -d "$CITYDB" -f "CREATE_DB.sql" -v srsno="$SRSNO" -v gmlsrsname="$GMLSRSNAME"
+echo
+echo 'Press ENTER to quit.'
+read
