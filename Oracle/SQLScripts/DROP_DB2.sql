0998e5f7796e9c3ca88b98158c355417b4efb6d7
Claus Nagel <cnagel@virtualcitysystems.de>
2018-05-12 20:30:23 +0200

    minor fix in Oracle DROP_DB

--- Oracle/SQLScripts/DROP_DB2.sql
+++ Oracle/SQLScripts/DROP_DB2.sql
@@ -28,14 +28,16 @@
 -- Disable versioning (if it was enabled before)
 @@DISABLE_VERSIONING2.sql
 
-SELECT 'Dropping 3DCityDB tables and user objects!' as message from DUAL;
+SELECT 'Dropping 3DCityDB tables and user objects' as message from DUAL;
 BEGIN
   FOR cur_rec IN (SELECT object_name, object_type
                     FROM user_objects
-                    WHERE object_type IN ('TABLE', 'VIEW', 'PACKAGE', 'PROCEDURE', 'FUNCTION', 'SEQUENCE')) LOOP
+                    WHERE object_type IN ('TABLE', 'VIEW', 'PACKAGE', 'PROCEDURE', 'FUNCTION', 'SEQUENCE', 'TYPE')) LOOP
     BEGIN
 	  IF cur_rec.object_type = 'TABLE' THEN
 	    EXECUTE IMMEDIATE 'DROP ' || cur_rec.object_type || ' "' || cur_rec.object_name || '" CASCADE CONSTRAINTS';
+	  ELSIF cur_rec.object_type = 'TYPE' THEN
+	    EXECUTE IMMEDIATE 'DROP ' || cur_rec.object_type || ' "' || cur_rec.object_name || '" FORCE';
       ELSE
 	    EXECUTE IMMEDIATE 'DROP ' || cur_rec.object_type || ' "' || cur_rec.object_name || '"';
       END IF;
