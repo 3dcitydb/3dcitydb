20328a510904a624cd89f7ab6dc5f08cd53c5637
Claus Nagel <cnagel@virtualcitysystems.de>
2018-05-29 14:13:34 +0200

    test

---
+++ Oracle/ShellScripts/Windows/CONNECTION_DETAILS.bat
@@ -0,0 +1,7 @@
+:: Provide your database details here -----------------------------------------
+set SQLPLUSBIN=D:\software\sqlplus\instantclient_12_2
+set HOST=oracletest
+set PORT=1521
+set SID=pdborcl
+set USERNAME=aaa
+::-----------------------------------------------------------------------------
---
+++ PostgreSQL/ShellScripts/Windows/CONNECTION_DETAILS.bat
@@ -0,0 +1,7 @@
+:: Provide your database details here -----------------------------------------
+set PGBIN=C:\Program Files\PostgreSQL\10\bin
+set PGHOST=localhost
+set PGPORT=5432
+set CITYDB=devel
+set PGUSER=cnagel
+::-----------------------------------------------------------------------------
