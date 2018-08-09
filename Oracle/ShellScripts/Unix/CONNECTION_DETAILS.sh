eafa051468574591bcb265c39ad832d7716feb8d
Claus Nagel <cnagel@virtualcitysystems.de>
2018-08-09 19:13:27 +0200

    minor change

--- Oracle/ShellScripts/Unix/CONNECTION_DETAILS.sh
+++ Oracle/ShellScripts/Unix/CONNECTION_DETAILS.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# Provide your database details here ------------------------------------------
+# Provide your database details here -----------------------------------
 export SQLPLUSBIN=path_to_sqlplus
 export HOST=your_host_address
 export PORT=1521
 export SID=your_SID_or_database_name
 export USERNAME=your_username
-# -----------------------------------------------------------------------------
+# ----------------------------------------------------------------------
 
-# Provide optional database details here --------------------------------------
-# The scripts GRANT_RO_ACCESS and REVOKE_RO_ACCESS require SYSDBA privileges
+# Provide optional database details here -------------------------------
+# The scripts GRANT_ACCESS and REVOKE_ACCESS require SYSDBA privileges
 export SYSDBA_USERNAME=
-#------------------------------------------------------------------------------
+#-----------------------------------------------------------------------
--- Oracle/ShellScripts/Windows/CONNECTION_DETAILS.bat
+++ Oracle/ShellScripts/Windows/CONNECTION_DETAILS.bat
@@ -1,12 +1,12 @@
-:: Provide your database details here -----------------------------------------
+:: Provide your database details here -----------------------------------
 set SQLPLUSBIN=path_to_sqlplus
 set HOST=your_host_address
 set PORT=1521
 set SID=your_SID_or_database_name
 set USERNAME=your_username
-::-----------------------------------------------------------------------------
+::-----------------------------------------------------------------------
 
-:: Provide optional database details here -------------------------------------
-:: The scripts GRANT_RO_ACCESS and REVOKE_RO_ACCESS require SYSDBA privileges
+:: Provide optional database details here -------------------------------
+:: The scripts GRANT_ACCESS and REVOKE_ACCESS require SYSDBA privileges
 set SYSDBA_USERNAME=
-::-----------------------------------------------------------------------------
+::-----------------------------------------------------------------------
