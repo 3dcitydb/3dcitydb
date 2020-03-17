10eec96097162b26c8e31bbdcc43e17b36a2cb0d
Felix Kunde <felix-kunde@gmx.de>
2020-03-17 22:11:45 +0100

    update year in copyright

--- PostgreSQL/ShellScripts/Unix/CONNECTION_DETAILS.sh
+++ PostgreSQL/ShellScripts/Unix/CONNECTION_DETAILS.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Provide your database details here ------------------------------------------
-export PGBIN=path_to_psql
-export PGHOST=your_host_address
+export PGBIN=
+export PGHOST=localhost
 export PGPORT=5432
-export CITYDB=your_database
-export PGUSER=your_username
+export CITYDB=pgm06
+export PGUSER=flydix
 #------------------------------------------------------------------------------
