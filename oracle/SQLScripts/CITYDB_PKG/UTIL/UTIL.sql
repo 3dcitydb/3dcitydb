cfcc4a5d0b4688d61fb44ef02fdcf393bdc125c2
Claus Nagel <cnagel@virtualcitysystems.de>
2021-04-26 20:27:42 +0200

    updated source file headers

--- oracle/SQLScripts/CITYDB_PKG/UTIL/UTIL.sql
+++ oracle/SQLScripts/CITYDB_PKG/UTIL/UTIL.sql
@@ -1,32 +1,29 @@
 -- 3D City Database - The Open Source CityGML Database
--- http://www.3dcitydb.org/
--- 
--- Copyright 2013 - 2020
+-- https://www.3dcitydb.org/
+--
+-- Copyright 2013 - 2021
 -- Chair of Geoinformatics
 -- Technical University of Munich, Germany
--- https://www.gis.bgu.tum.de/
--- 
+-- https://www.lrg.tum.de/gis/
+--
 -- The 3D City Database is jointly developed with the following
 -- cooperation partners:
--- 
--- virtualcitySYSTEMS GmbH, Berlin <http://www.virtualcitysystems.de/>
+--
+-- Virtual City Systems, Berlin <https://vc.systems/>
 -- M.O.S.S. Computer Grafik Systeme GmbH, Taufkirchen <http://www.moss.de/>
--- 
+--
 -- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
 -- You may obtain a copy of the License at
--- 
+--
 --     http://www.apache.org/licenses/LICENSE-2.0
---     
+--
 -- Unless required by applicable law or agreed to in writing, software
 -- distributed under the License is distributed on an "AS IS" BASIS,
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 --
-
---SET term OFF;
---SET serveroutput OFF;
 
 /*****************************************************************
 * TYPE STRARRAY
