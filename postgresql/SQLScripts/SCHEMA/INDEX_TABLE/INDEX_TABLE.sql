028b533945b9f7a50a00c02bd33fed8510a69a65
Zhihang Yao <zyao@virtualcitysystems.de>
2022-05-18 09:46:03 +0200

    cleanup for v5

--- postgresql/SQLScripts/SCHEMA/INDEX_TABLE/INDEX_TABLE.sql
+++ postgresql/SQLScripts/SCHEMA/INDEX_TABLE/INDEX_TABLE.sql
@@ -39,16 +39,9 @@ CREATE TABLE INDEX_TABLE (
 * Populate INDEX_TABLE with INDEX_OBJ instances
 * 
 ******************************************************************/
-INSERT INTO index_table (obj) VALUES (citydb_pkg.construct_spatial_2d('cityobject_envelope_spx', 'cityobject', 'envelope'));
 INSERT INTO index_table (obj) VALUES (citydb_pkg.construct_spatial_2d('surface_geom_spx', 'surface_geometry', 'geometry'));
 INSERT INTO index_table (obj) VALUES (citydb_pkg.construct_spatial_2d('surface_geom_solid_spx', 'surface_geometry', 'solid_geometry'));
-INSERT INTO index_table (obj) VALUES (citydb_pkg.construct_normal('cityobject_inx', 'cityobject', 'gmlid, gmlid_codespace'));
-INSERT INTO index_table (obj) VALUES (citydb_pkg.construct_normal('cityobject_lineage_inx', 'cityobject', 'lineage'));
-INSERT INTO index_table (obj) VALUES (citydb_pkg.construct_normal('cityobj_creation_date_inx', 'cityobject', 'creation_date'));
-INSERT INTO index_table (obj) VALUES (citydb_pkg.construct_normal('cityobj_term_date_inx', 'cityobject', 'termination_date'));
-INSERT INTO index_table (obj) VALUES (citydb_pkg.construct_normal('cityobj_last_mod_date_inx', 'cityobject', 'last_modification_date'));
 INSERT INTO index_table (obj) VALUES (citydb_pkg.construct_normal('surface_geom_inx', 'surface_geometry', 'gmlid, gmlid_codespace'));
 INSERT INTO index_table (obj) VALUES (citydb_pkg.construct_normal('appearance_inx', 'appearance', 'gmlid, gmlid_codespace'));
 INSERT INTO index_table (obj) VALUES (citydb_pkg.construct_normal('appearance_theme_inx', 'appearance', 'theme'));
 INSERT INTO index_table (obj) VALUES (citydb_pkg.construct_normal('surface_data_inx', 'surface_data', 'gmlid, gmlid_codespace'));
-INSERT INTO index_table (obj) VALUES (citydb_pkg.construct_normal('address_inx', 'address', 'gmlid, gmlid_codespace'));
