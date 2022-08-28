a6298c52aeecf33bcac12f93ec2350cda862b77b
erchen.cz <erchen.cz@alibaba-inc.com>
2022-08-28 21:53:06 +0800

    Fix according to yaozhihang's comments: 1. Add a missing raster type hint for checking raster type in create spatial database 2. Change PolarDB in Readme file to a fixed number.

--- postgresql/SQLScripts/CREATE_DB.sql
+++ postgresql/SQLScripts/CREATE_DB.sql
@@ -39,10 +39,10 @@ SELECT EXISTS(SELECT 1 AS create_raster from pg_type where typname= 'raster') AS
 --// break if the raster data type is not available
 SELECT CASE WHEN :'raster_type_exists' = 't'
   THEN 'UTIL/HINTS/DO_NOTHING.sql'
-  ELSE 'UTIL/HINTS/HINT_ON_MISSING_RASTER_EXTENSION.sql'
-  END AS do_action_for_raster_extension_check;
+  ELSE 'UTIL/HINTS/HINT_ON_MISSING_RASTER_TYPE.sql'
+  END AS do_action_for_raster_tye_check;
 \gset
-\ir :do_action_for_raster_extension_check
+\ir :do_action_for_raster_type_check
 
 --// create schema
 CREATE SCHEMA citydb;
