bbbf02ae09083032cbfc09b5d7112de29011f174
Zhihang Yao <zyao@virtualcitysystems.de>
2022-11-18 14:12:57 +0100

    minor change to the default PGPORT of connection details

--- oracle/ShellScripts/Windows/CONNECTION_DETAILS.bat
+++ oracle/ShellScripts/Windows/CONNECTION_DETAILS.bat
@@ -1,7 +1,7 @@
 :: Provide your database details here -----------------------------------
 set SQLPLUSBIN=path_to_sqlplus
 set HOST=your_host_address
-set PORT=1521
+set PORT=your_db_port(e.g. 1521)
 set SID=your_SID_or_database_name
 set USERNAME=your_username
 ::-----------------------------------------------------------------------
--- postgresql/ShellScripts/Windows/CONNECTION_DETAILS.bat
+++ postgresql/ShellScripts/Windows/CONNECTION_DETAILS.bat
@@ -1,7 +1,7 @@
 :: Provide your database details here -----------------------------------------
 set PGBIN=path_to_psql
 set PGHOST=your_host_address
-set PGPORT=5432
+set PGPORT=your_db_port(e.g. 5432)
 set CITYDB=your_database
 set PGUSER=your_username
 ::-----------------------------------------------------------------------------
