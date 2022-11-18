64bf8a5266afa9ff19fa18ba8db3972b51d5f381
Zhihang Yao <zyao@virtualcitysystems.de>
2022-11-18 14:32:37 +0100

    complement the previous commit

--- oracle/ShellScripts/Unix/CONNECTION_DETAILS.sh
+++ oracle/ShellScripts/Unix/CONNECTION_DETAILS.sh
@@ -2,7 +2,7 @@
 # Provide your database details here -----------------------------------
 export SQLPLUSBIN=path_to_sqlplus
 export HOST=your_host_address
-export PORT=1521
+export PORT=your_db_port(e.g. 1521)
 export SID=your_SID_or_database_name
 export USERNAME=your_username
 # ----------------------------------------------------------------------
--- postgresql/ShellScripts/Unix/CONNECTION_DETAILS.sh
+++ postgresql/ShellScripts/Unix/CONNECTION_DETAILS.sh
@@ -2,7 +2,7 @@
 # Provide your database details here ------------------------------------------
 export PGBIN=path_to_psql
 export PGHOST=your_host_address
-export PGPORT=5432
+export PGPORT=your_db_port(e.g. 5432)
 export CITYDB=your_database
 export PGUSER=your_username
 #------------------------------------------------------------------------------
