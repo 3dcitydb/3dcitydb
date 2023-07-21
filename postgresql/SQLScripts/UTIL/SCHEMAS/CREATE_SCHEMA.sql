1a6dda5580bc67fb52ed537b3413afdebf34698a
Zhihang Yao <zyao@virtualcitysystems.de>
2023-07-21 08:27:43 +0200

    fixed missing datatype instance when creating a new schema

--- postgresql/SQLScripts/UTIL/SCHEMAS/CREATE_SCHEMA.sql
+++ postgresql/SQLScripts/UTIL/SCHEMAS/CREATE_SCHEMA.sql
@@ -55,6 +55,7 @@ SELECT version as citydb_version from citydb_pkg.citydb_version();
 --// fill metadata tables
 \ir ../../SCHEMA/METADATA/NAMESPACE_INSTANCES.sql
 \ir ../../SCHEMA/METADATA/OBJECTCLASS_INSTANCES.sql
+\ir ../../SCHEMA/METADATA/DATATYPE_INSTANCES.sql
 \ir ../../SCHEMA/METADATA/AGGREGATION_INFO_INSTANCES.sql
 
 --// fill codelist tables
