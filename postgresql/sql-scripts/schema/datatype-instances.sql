5f59802e7a006f73b004287129c3c337af1cc2ed
Claus Nagel <cnagel@virtualcitysystems.de>
2025-02-22 13:24:12 +0100

    added TimePosition and Duration data types

--- postgresql/sql-scripts/schema/datatype-instances.sql
+++ postgresql/sql-scripts/schema/datatype-instances.sql
@@ -1,4 +1,5 @@
-DELETE FROM datatype;
+DELETE
+FROM datatype;
 
 -- Core Module --
 
@@ -69,52 +70,58 @@ INSERT INTO datatype (ID, SUPERTYPE_ID, TYPENAME, IS_ABSTRACT, NAMESPACE_ID, SCH
 VALUES (22, null, 'StringOrRef', 0, 1, @core:StringOrRef@);
 
 INSERT INTO datatype (ID, SUPERTYPE_ID, TYPENAME, IS_ABSTRACT, NAMESPACE_ID, SCHEMA)
-VALUES (23, null, 'ADEOfAbstractCityObject', 1, 1, @core:ADEOfAbstractCityObject@);
+VALUES (23, null, 'TimePosition', 0, 1, @core:TimePosition@);
 
 INSERT INTO datatype (ID, SUPERTYPE_ID, TYPENAME, IS_ABSTRACT, NAMESPACE_ID, SCHEMA)
-VALUES (24, null, 'ADEOfAbstractDynamizer', 1, 1, @core:ADEOfAbstractDynamizer@);
+VALUES (24, null, 'Duration', 0, 1, @core:Duration@);
 
 INSERT INTO datatype (ID, SUPERTYPE_ID, TYPENAME, IS_ABSTRACT, NAMESPACE_ID, SCHEMA)
-VALUES (25, null, 'ADEOfAbstractFeature', 1, 1, @core:ADEOfAbstractFeature@);
+VALUES (50, null, 'ADEOfAbstractCityObject', 1, 1, @core:ADEOfAbstractCityObject@);
 
 INSERT INTO datatype (ID, SUPERTYPE_ID, TYPENAME, IS_ABSTRACT, NAMESPACE_ID, SCHEMA)
-VALUES (26, null, 'ADEOfAbstractFeatureWithLifespan', 1, 1, @core:ADEOfAbstractFeatureWithLifespan@);
+VALUES (51, null, 'ADEOfAbstractDynamizer', 1, 1, @core:ADEOfAbstractDynamizer@);
 
 INSERT INTO datatype (ID, SUPERTYPE_ID, TYPENAME, IS_ABSTRACT, NAMESPACE_ID, SCHEMA)
-VALUES (27, null, 'ADEOfAbstractLogicalSpace', 1, 1, @core:ADEOfAbstractLogicalSpace@);
+VALUES (52, null, 'ADEOfAbstractFeature', 1, 1, @core:ADEOfAbstractFeature@);
 
 INSERT INTO datatype (ID, SUPERTYPE_ID, TYPENAME, IS_ABSTRACT, NAMESPACE_ID, SCHEMA)
-VALUES (28, null, 'ADEOfAbstractOccupiedSpace', 1, 1, @core:ADEOfAbstractOccupiedSpace@);
+VALUES (53, null, 'ADEOfAbstractFeatureWithLifespan', 1, 1, @core:ADEOfAbstractFeatureWithLifespan@);
 
 INSERT INTO datatype (ID, SUPERTYPE_ID, TYPENAME, IS_ABSTRACT, NAMESPACE_ID, SCHEMA)
-VALUES (29, null, 'ADEOfAbstractPhysicalSpace', 1, 1, @core:ADEOfAbstractPhysicalSpace@);
+VALUES (54, null, 'ADEOfAbstractLogicalSpace', 1, 1, @core:ADEOfAbstractLogicalSpace@);
 
 INSERT INTO datatype (ID, SUPERTYPE_ID, TYPENAME, IS_ABSTRACT, NAMESPACE_ID, SCHEMA)
-VALUES (30, null, 'ADEOfAbstractPointCloud', 1, 1, @core:ADEOfAbstractPointCloud@);
+VALUES (55, null, 'ADEOfAbstractOccupiedSpace', 1, 1, @core:ADEOfAbstractOccupiedSpace@);
 
 INSERT INTO datatype (ID, SUPERTYPE_ID, TYPENAME, IS_ABSTRACT, NAMESPACE_ID, SCHEMA)
-VALUES (31, null, 'ADEOfAbstractSpace', 1, 1, @core:ADEOfAbstractSpace@);
+VALUES (56, null, 'ADEOfAbstractPhysicalSpace', 1, 1, @core:ADEOfAbstractPhysicalSpace@);
 
 INSERT INTO datatype (ID, SUPERTYPE_ID, TYPENAME, IS_ABSTRACT, NAMESPACE_ID, SCHEMA)
-VALUES (32, null, 'ADEOfAbstractSpaceBoundary', 1, 1, @core:ADEOfAbstractSpaceBoundary@);
+VALUES (57, null, 'ADEOfAbstractPointCloud', 1, 1, @core:ADEOfAbstractPointCloud@);
 
 INSERT INTO datatype (ID, SUPERTYPE_ID, TYPENAME, IS_ABSTRACT, NAMESPACE_ID, SCHEMA)
-VALUES (33, null, 'ADEOfAbstractThematicSurface', 1, 1, @core:ADEOfAbstractThematicSurface@);
+VALUES (58, null, 'ADEOfAbstractSpace', 1, 1, @core:ADEOfAbstractSpace@);
 
 INSERT INTO datatype (ID, SUPERTYPE_ID, TYPENAME, IS_ABSTRACT, NAMESPACE_ID, SCHEMA)
-VALUES (34, null, 'ADEOfAbstractUnoccupiedSpace', 1, 1, @core:ADEOfAbstractUnoccupiedSpace@);
+VALUES (59, null, 'ADEOfAbstractSpaceBoundary', 1, 1, @core:ADEOfAbstractSpaceBoundary@);
 
 INSERT INTO datatype (ID, SUPERTYPE_ID, TYPENAME, IS_ABSTRACT, NAMESPACE_ID, SCHEMA)
-VALUES (35, null, 'ADEOfAbstractVersion', 1, 1, @core:ADEOfAbstractVersion@);
+VALUES (60, null, 'ADEOfAbstractThematicSurface', 1, 1, @core:ADEOfAbstractThematicSurface@);
 
 INSERT INTO datatype (ID, SUPERTYPE_ID, TYPENAME, IS_ABSTRACT, NAMESPACE_ID, SCHEMA)
-VALUES (36, null, 'ADEOfAbstractVersionTransition', 1, 1, @core:ADEOfAbstractVersionTransition@);
+VALUES (61, null, 'ADEOfAbstractUnoccupiedSpace', 1, 1, @core:ADEOfAbstractUnoccupiedSpace@);
 
 INSERT INTO datatype (ID, SUPERTYPE_ID, TYPENAME, IS_ABSTRACT, NAMESPACE_ID, SCHEMA)
-VALUES (37, null, 'ADEOfCityModel', 1, 1, @core:ADEOfCityModel@);
+VALUES (62, null, 'ADEOfAbstractVersion', 1, 1, @core:ADEOfAbstractVersion@);
 
 INSERT INTO datatype (ID, SUPERTYPE_ID, TYPENAME, IS_ABSTRACT, NAMESPACE_ID, SCHEMA)
-VALUES (38, null, 'ADEOfClosureSurface', 1, 1, @core:ADEOfClosureSurface@);
+VALUES (63, null, 'ADEOfAbstractVersionTransition', 1, 1, @core:ADEOfAbstractVersionTransition@);
+
+INSERT INTO datatype (ID, SUPERTYPE_ID, TYPENAME, IS_ABSTRACT, NAMESPACE_ID, SCHEMA)
+VALUES (64, null, 'ADEOfCityModel', 1, 1, @core:ADEOfCityModel@);
+
+INSERT INTO datatype (ID, SUPERTYPE_ID, TYPENAME, IS_ABSTRACT, NAMESPACE_ID, SCHEMA)
+VALUES (65, null, 'ADEOfClosureSurface', 1, 1, @core:ADEOfClosureSurface@);
 
 -- Dynamizer Module --
 
