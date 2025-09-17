03fb40215474e7dabd6a43fda7f83cbf527dc03a
Claus Nagel <cnagel@virtualcitysystems.de>
2025-09-17 10:23:57 +0200

    minor changes to changelog scripts

--- postgresql/sql-scripts/create-changelog.sql
+++ postgresql/sql-scripts/create-changelog.sql
@@ -3,80 +3,43 @@
 \echo 'Setting up changelog extension ...'
 
 \set SCHEMA_NAME :schema_name
+\set SEQ_NAME :SCHEMA_NAME.feature_changelog_seq
 
--- Tables
+CREATE SEQUENCE :SCHEMA_NAME.feature_changelog_seq START WITH 1 INCREMENT BY 1 MINVALUE 0 MAXVALUE 9223372036854775807 CACHE 1 NO CYCLE;
+
 CREATE TABLE :SCHEMA_NAME.feature_changelog (
-	id bigint NOT NULL,
-	feature_id bigint,
+  "id" bigint DEFAULT nextval(:'SEQ_NAME'::regclass) NOT NULL,
+  feature_id bigint,
   objectclass_id integer NOT NULL,
   objectid text,
   identifier text,
-	identifier_codespace text,
-	envelope geometry(POLYGONZ),
+  identifier_codespace text,
+  envelope geometry(POLYGONZ),
   transaction_type text NOT NULL,
   transaction_date timestamp with time zone,
   db_user text,
   reason_for_update text,
-  CONSTRAINT feature_changelog_pk PRIMARY KEY (id)
-  WITH (FILLFACTOR=100)
+  CONSTRAINT feature_changelog_pk PRIMARY KEY ( id ) WITH ( FILLFACTOR = 100 )
 );
 
--- Sequences
-CREATE SEQUENCE :SCHEMA_NAME.feature_changelog_seq
-	INCREMENT BY 1
-	MINVALUE 0
-	MAXVALUE 9223372036854775807
-	START WITH 1
-	CACHE 1
-	NO CYCLE
-	OWNED BY NONE;
+ALTER TABLE :SCHEMA_NAME.feature_changelog ADD CONSTRAINT feature_changelog_feature_fk
+  FOREIGN KEY ( feature_id ) REFERENCES :SCHEMA_NAME.feature ( id )
+  ON DELETE SET NULL ON UPDATE NO ACTION;
 
--- Constraints
-ALTER TABLE :SCHEMA_NAME.feature_changelog ADD CONSTRAINT feature_changelog_fk1 FOREIGN KEY (feature_id)
-REFERENCES :SCHEMA_NAME.feature (id) MATCH FULL
-ON DELETE SET NULL ON UPDATE NO ACTION;
+CREATE INDEX feature_changelog_feature_fkx ON :SCHEMA_NAME.feature_changelog ( feature_id ) WITH ( FILLFACTOR = 90 );
 
--- Indexes
-CREATE INDEX feature_changelog_fkx1 ON :SCHEMA_NAME.feature_changelog
-	USING btree
-	(
-	  feature_id ASC NULLS LAST
-	)	WITH (FILLFACTOR = 90);
+CREATE INDEX feature_changelog_objectclass_inx ON :SCHEMA_NAME.feature_changelog ( objectclass_id ) WITH ( FILLFACTOR = 90 );
 
-CREATE INDEX feature_changelog_fkx2 ON :SCHEMA_NAME.feature_changelog
-	USING btree
-	(
-	  objectclass_id ASC NULLS LAST
-	)	WITH (FILLFACTOR = 90);
+CREATE INDEX feature_changelog_objectid_inx ON :SCHEMA_NAME.feature_changelog ( objectid ) WITH ( FILLFACTOR = 90 );
 
-CREATE INDEX feature_changelog_inx1 ON :SCHEMA_NAME.feature_changelog
-	USING btree
-	(
-	  objectid ASC NULLS LAST
-	)	WITH (FILLFACTOR = 90);
+CREATE INDEX feature_changelog_identifier_inx ON :SCHEMA_NAME.feature_changelog ( identifier, identifier_codespace ) WITH ( FILLFACTOR = 90 );
 
-CREATE INDEX feature_changelog_inx2 ON :SCHEMA_NAME.feature_changelog
-	USING btree
-	(
-	  identifier ASC NULLS LAST,
-	  identifier_codespace
-	)	WITH (FILLFACTOR = 90);
+CREATE INDEX feature_changelog_transaction_date_inx ON :SCHEMA_NAME.feature_changelog ( transaction_date ) WITH ( FILLFACTOR = 100 );
 
-CREATE INDEX feature_changelog_inx3 ON :SCHEMA_NAME.feature_changelog
-	USING btree
-	(
-	  transaction_date ASC NULLS LAST
-	)	WITH (FILLFACTOR = 90);
+CREATE INDEX feature_changelog_envelope_spx ON :SCHEMA_NAME.feature_changelog USING gist ( envelope );
 
-CREATE INDEX feature_changelog_spx ON :SCHEMA_NAME.feature_changelog
-	USING gist
-	(
-	  envelope
-	);
-
--- Functions
 CREATE OR REPLACE FUNCTION :SCHEMA_NAME.log_feature_changes() RETURNS TRIGGER AS
-$feature_changelog_trigger$
+$body$
 DECLARE
   rec RECORD;
 	feature_id bigint;
@@ -116,9 +79,8 @@ BEGIN
 
 	IF (is_toplevel = 1) THEN
 		IF (transaction_type = 'UPDATE' AND OLD.last_modification_date <> NEW.last_modification_date) OR (transaction_type <> 'UPDATE') THEN
-            EXECUTE format('insert into %I.feature_changelog
-                                select nextval(''%I.feature_changelog_seq''::regclass),
-                                        $1, $2, $3, $4, $5, $6, $7, $8, $9', TG_TABLE_SCHEMA, TG_TABLE_SCHEMA)
+            EXECUTE format('insert into %I.feature_changelog (feature_id, objectclass_id, objectid, identifier, identifier_codespace, envelope, transaction_type, transaction_date, db_user, reason_for_update)
+                                select $1, $2, $3, $4, $5, $6, $7, $8, $9, $10', TG_TABLE_SCHEMA, TG_TABLE_SCHEMA)
             USING feature_id, objectclass_id, objectid, identifier, identifier_codespace, envelope, transaction_type, now(), user, reason_for_update;
 		END IF;
 	ELSIF (transaction_type = 'UPDATE' AND OLD.last_modification_date <> NEW.last_modification_date) THEN
@@ -132,12 +94,11 @@ BEGIN
 
 	RETURN NULL;
 END;
-$feature_changelog_trigger$ LANGUAGE plpgsql;
+$body$ LANGUAGE plpgsql;
 
--- Triggers
 CREATE TRIGGER feature_changelog_trigger
-AFTER INSERT OR UPDATE OR DELETE ON :SCHEMA_NAME.feature
+  AFTER INSERT OR UPDATE OR DELETE ON :SCHEMA_NAME.feature
   FOR EACH ROW EXECUTE PROCEDURE :SCHEMA_NAME.log_feature_changes();
 
 \echo
-\echo 'Changelog extension created!'
+\echo 'Changelog extension successfully created.'
--- postgresql/sql-scripts/drop-db-changelog.sql
+++ postgresql/sql-scripts/drop-db-changelog.sql
@@ -1,3 +1,4 @@
+SET client_min_messages TO WARNING;
 \set ON_ERROR_STOP ON
 
 \echo
@@ -5,9 +6,9 @@
 
 \set SCHEMA_NAME :schema_name
 
-DROP SEQUENCE IF EXISTS :SCHEMA_NAME.feature_changelog_seq;
 DROP TABLE IF EXISTS :SCHEMA_NAME.feature_changelog;
+DROP SEQUENCE IF EXISTS :SCHEMA_NAME.feature_changelog_seq;
 DROP TRIGGER IF EXISTS feature_changelog_trigger on :SCHEMA_NAME.feature;
 
 \echo
-\echo 'Changelog extension successfully removed!'
+\echo 'Changelog extension successfully removed.'
