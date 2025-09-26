d191774ad8bde959fc1cb84f4f2dca02a64fcf8e
Karin Patenge <37591997+karinpatenge@users.noreply.github.com>
2025-09-26 14:33:51 +0200

    connection-details.sh created
    For UNIX/Linux

---
+++ oracle/shell-scripts/connection-details.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Provide your database details here ------------------------------------------
+export DBHOST=your_host
+export DBPORT=your_port
+export ORACLE_PDB=your_pdb
+export DBUSER=citydb_username
+#------------------------------------------------------------------------------
