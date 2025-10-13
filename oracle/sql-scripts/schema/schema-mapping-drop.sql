1a481d1871ce3d8d5ebdc13a21e1f910a4d74eab
Karin Patenge <37591997+karinpatenge@users.noreply.github.com>
2025-10-13 10:11:04 +0200

    Remove procedure needed to insert JSON schema mapping

---
+++ oracle/sql-scripts/schema/schema-mapping-drop.sql
@@ -0,0 +1,10 @@
+-----------------------------------------------------
+-- Author: Karin Patenge, Oracle
+-- Last update: Okt 13, 2025
+-- Status: to be reviewed
+-- This scripts requires Oracle Database version 23ai
+-----------------------------------------------------
+
+-- Remove PL/SQL procedures to insert JSON values in the 3DCityDB metadata
+DROP PROCEDURE map_objectclass_schema;
+DROP PROCEDURE map_datatype_schema;
