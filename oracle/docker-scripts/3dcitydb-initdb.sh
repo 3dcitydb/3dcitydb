da2994cc3260d72db51af51ad8f7de86c78d50af
Claus Nagel <cnagel@virtualcitysystems.de>
2025-10-26 21:40:33 +0100

    fixed Oracle Docker scripts

--- oracle/docker-scripts/3dcitydb-initdb.sh
+++ oracle/docker-scripts/3dcitydb-initdb.sh
@@ -1,27 +1,20 @@
 #!/usr/bin/env bash
 # 3DCityDB setup --------------------------------------------------------------
 
-# Print commands and their arguments as they are executed
-set -e;
+# Exit on error
+set -e
 
 # Set 3DCityDB version --------------------------------------------------------
 if [ -z $CITYDB_VERSION ]; then
   # CITYDB_VERSION unset, read version from the version.txt file
-  read -r CITYDB_VERSION < version.txt
+  CITYDB_VERSION=$(< version.txt)
 fi
 
 # ORACLE_PWD ------------------------------------------------------------------
-if [ -z ${ORACLE_PWD+x} ]; then
+if [ -z $ORACLE_PWD ]; then
   echo
-  echo "Password (ORACLE_PWD) must be set for Oracle Database users."
-  exit
-fi
-
-# ORACLE_PDB ------------------------------------------------------------------
-if [ -z ${ORACLE_PDB+x} ]; then
-  ORACLE_PDB="FREEPDB1"
-else
-  ORACLE_PDB="$ORACLE_PDB"
+  echo "ORACLE_PWD is not set. No 3DCityDB instance will be created."
+  exit 1
 fi
 
 # SRID ------------------------------------------------------------------------
@@ -30,12 +23,12 @@ if [ -z ${SRID+x} ]; then
   # No SRID set -> give instructions on how to create a DB and do nothing
   echo
   echo "SRID is not set. No 3DCityDB instance will be created."
-  exit
+  exit 1
 else
   # SRID given, check if valid
   if [[ ! $SRID =~ $regex_numeric ]] || [ $SRID -le 0 ]; then
         echo
-        echo 'Illegal input! Enter a positive integer for the SRID.'
+        echo 'Illegal input. Enter a positive integer for the SRID.'
   fi
 fi
 
@@ -57,99 +50,82 @@ else
   if [ ! -z ${HEIGHT_EPSG+x} ]; then
     # SRS_NAME is set, HEIGHT_EPSG is ignored
     echo
-    echo "!!! WARNING: SRS_NAME is set. HEIGHT_EPSG will be ignored."
+    echo "WARNING: SRS_NAME is set. HEIGHT_EPSG will be ignored."
   fi
 fi
 
-# DBUSER ----------------------------------------------------------------------
-if [ -z ${DBUSER+x} ]; then
-  DBUSER="citydb"
-else
-  DBUSER="$DBUSER"
-fi
-
-# DBHOST ----------------------------------------------------------------------
-if [ -z ${DBHOST+x} ]; then
-  DBHOST="localhost"
-else
-  DBHOST="$DBHOST"
-fi
-
-# DBPORT ----------------------------------------------------------------------
-if [ -z ${DBPORT+x} ]; then
-  DBPORT="1521"
-else
-  DBPORT="$DBPORT"
-fi
-
-# TABLESPACE ------------------------------------------------------------------
-if [ -z ${TABLESPACE+x} ]; then
-  TABLESPACE="users"
-else
-  TABLESPACE="$TABLESPACE"
-fi
-
 # CHANGELOG -------------------------------------------------------------------
-#if [ -z ${CHANGELOG+x} ]; then
-#  CHANGELOG="no"
-#else
-#  CHANGELOG="$CHANGELOG"
-#fi
+if [ -z ${CHANGELOG+x} ]; then
+  CHANGELOG="no"
+fi
+
+# ORACLE_PDB ------------------------------------------------------------------
+if [ -z ${ORACLE_PDB+x} ]; then
+  ORACLE_PDB="FREEPDB1"
+fi
+
+# DB_USER ---------------------------------------------------------------------
+if [ -z ${DB_USER+x} ]; then
+  DB_USER="citydb"
+fi
+
+# Read Oracle version ---------------------------------------------------------
+ORACLE_VERSION=$(sqlplus -S -L system/"$ORACLE_PWD"@"$ORACLE_PDB" <<-EOF
+  SET PAGESIZE 0 FEEDBACK OFF VERIFY OFF HEADING OFF ECHO OFF
+  SELECT banner FROM v\$version;
+EOF
+)
 
 # Create user -----------------------------------------------------------------
 echo
-echo "Creating user $DBUSER ..."
-echo "CREATE USER IF NOT EXISTS $DBUSER identified by $ORACLE_PWD;
-      ALTER USER $DBUSER QUOTA UNLIMITED ON $TABLESPACE;
-      GRANT CONNECT, RESOURCE to $DBUSER;
-      GRANT CREATE SESSION TO $DBUSER;
-      GRANT DB_DEVELOPER_ROLE TO $DBUSER;" | sqlplus system/"$ORACLE_PWD"@"$DBHOST":"$DBPORT"/"$ORACLE_PDB"
-echo "Creating user $DBUSER ... done!"
-echo
+echo "Creating user '$DB_USER' ..."
+sqlplus -S -L system/"$ORACLE_PWD"@"$ORACLE_PDB" <<-EOF > /dev/null
+  CREATE USER IF NOT EXISTS $DB_USER identified by $ORACLE_PWD;
+  ALTER USER $DB_USER QUOTA UNLIMITED ON USERS;
+  GRANT CONNECT, RESOURCE to $DB_USER;
+  GRANT CREATE SESSION TO $DB_USER;
+  GRANT DB_DEVELOPER_ROLE TO $DB_USER;
+EOF
+echo "Creating user '$DB_USER' done."
 
 # Setup 3DCityDB schema -------------------------------------------------------
 echo
-echo "Setting up 3DCityDB database schema in database $DBUSER ..."
-sqlplus -S -L "$DBUSER"/"$ORACLE_PWD"@"$DBHOST":"$DBPORT"/"$ORACLE_PDB" @CREATE_DB.sql "${SRID}" "${SRSNAME}"
-echo "Setting up 3DCityDB schema in $DBUSER ... done!"
-echo
-echo "# Setting up 3DCityDB ... done! ################################################"
+echo "Setting up 3DCityDB database schema for user '$DB_USER' ..."
+sqlplus -S -L "$DB_USER"/"$ORACLE_PWD"@"$ORACLE_PDB" @create-db.sql \
+  "${SRID}" "${SRS_NAME}" "${CHANGELOG}" > /dev/null
+echo "Setting up 3DCityDB database schema for user '$DB_USER' done."
 
 # Echo info -------------------------------------------------------------------
 cat <<EOF
 
 
-# 3DCityDB Docker Oracle ######################################################
+###############################################################################
+#  _______   ___ _ _        ___  ___
+# |__ /   \ / __(_) |_ _  _|   \| _ )
+#  |_ \ |) | (__| |  _| || | |) | _ \\
+# |___/___/ \___|_|\__|\_, |___/|___/
+#                      |__/
 #
-# Oracle Version --------------------------------------------------------------
-#   Latest Oracle Database Enterprise Edition from Oracle Container Registry
-#   https://container-registry.oracle.com/database/enterprise:21.3.0.0
-#   !! 23ai not yet available !!
+# 3DCityDB Docker Oracle
 #
-#   Latest Oracle Database 23ai Free Edition from Oracle Container Registry
-#   https://container-registry.oracle.com/database/free:latest
+# Oracle ----------------------------------------------------------------------
+#   Oracle version      $ORACLE_VERSION
 #
 # 3DCityDB --------------------------------------------------------------------
 #   3DCityDB version    $CITYDB_VERSION
 #   ORACLE_PDB          $ORACLE_PDB
-#   DBHOST              $DBHOST
-#   DBPORT              $DBPORT
-#   DBUSER              $DBUSER
-#   ORACLE_PWD          $ORACLE_PWD
-#   TABLESPACE          $TABLESPACE
-#   VERSIONING enabled  $VERSIONING
-#   CHANGELOG enabled   $CHANGELOG
+#   DB_USER             $DB_USER
 #   SRID                $SRID
-#   SRSNAME             $SRS_NAME
+#   SRS_NAME            $SRS_NAME
 #   HEIGHT_EPSG         $HEIGHT_EPSG
+#   CHANGELOG enabled   $CHANGELOG
+#
+#   https://github.com/3dcitydb
 #
 # Maintainer ------------------------------------------------------------------
 #   Karin Patenge
 #   Oracle Global Services Deutschland GmbH
 #   karin.patenge(at)oracle.com
-#
-# Reviewers ------------------------------------------------------------------
-#
 #
 ################################################################################
 
