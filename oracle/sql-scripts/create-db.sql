977087b0c764baf335c2a1a7dd80b47d5aa6be18
Claus Nagel <cnagel@virtualcitysystems.de>
2025-10-30 14:20:49 +0100

    minor change

--- oracle/sql-scripts/create-db.sql
+++ oracle/sql-scripts/create-db.sql
@@ -9,14 +9,12 @@ DEFINE SRID=&1;
 DEFINE SRS_NAME=&2;
 DEFINE CHANGELOG=&3;
 VARIABLE V_SRID NUMBER;
-VARIABLE V_SRS_NAME VARCHAR2(4000);
 
 -- check if the provided SRID is supported
 PROMPT
 PROMPT Checking spatial reference system for SRID &SRID ...
 BEGIN
   SELECT SRID INTO :V_SRID FROM MDSYS.CS_SRS WHERE SRID = &SRID;
-  :V_SRS_NAME := '&SRS_NAME';
 EXCEPTION
   WHEN NO_DATA_FOUND THEN
     RAISE_APPLICATION_ERROR(-20001, 'The SRID ' || &SRID || ' is not supported. To add it manually, see CRS definitions at https://spatialreference.org/.');
