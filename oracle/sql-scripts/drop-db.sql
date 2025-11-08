e1b07cd5ceb6614c1ac8b82d3705201ad3a9cb91
Claus Nagel <cnagel@virtualcitysystems.de>
2025-11-08 11:50:43 +0100

    minor change

--- oracle/sql-scripts/drop-db.sql
+++ oracle/sql-scripts/drop-db.sql
@@ -8,18 +8,18 @@ WHENEVER SQLERROR EXIT
 PROMPT
 PROMPT Dropping 3DCityDB schema ...
 BEGIN
-  FOR cur_rec IN (
+  FOR rec IN (
     SELECT object_name, object_type
     FROM user_objects
     WHERE object_type IN ('TABLE', 'VIEW', 'PACKAGE', 'PROCEDURE', 'FUNCTION', 'SEQUENCE', 'TYPE')
   ) LOOP
     BEGIN
-      IF cur_rec.object_type = 'TABLE' THEN
-        EXECUTE IMMEDIATE 'DROP ' || cur_rec.object_type || ' "' || cur_rec.object_name || '" CASCADE CONSTRAINTS';
-      ELSIF cur_rec.object_type = 'TYPE' THEN
-        EXECUTE IMMEDIATE 'DROP ' || cur_rec.object_type || ' "' || cur_rec.object_name || '" FORCE';
+      IF rec.object_type = 'TABLE' THEN
+        EXECUTE IMMEDIATE 'DROP ' || rec.object_type || ' "' || rec.object_name || '" CASCADE CONSTRAINTS';
+      ELSIF rec.object_type = 'TYPE' THEN
+        EXECUTE IMMEDIATE 'DROP ' || rec.object_type || ' "' || rec.object_name || '" FORCE';
       ELSE
-        EXECUTE IMMEDIATE 'DROP ' || cur_rec.object_type || ' "' || cur_rec.object_name || '"';
+        EXECUTE IMMEDIATE 'DROP ' || rec.object_type || ' "' || rec.object_name || '"';
       END IF;
     EXCEPTION
 	    WHEN OTHERS THEN
